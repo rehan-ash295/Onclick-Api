746a9a820102c061f8784d89dfc3edcae77f33e8
rehan-ash295 <rehanashfaque.303@gmail.com>
2025-05-19 13:22:21 +0530

    Debounce use case

---
+++ vite-project/src/use-Debounce.js
@@ -0,0 +1,59 @@
+import { useEffect, useRef, useState, createContext, useContext } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+
+
+
+const useDebounce =(data, delay)=>{
+  const [debounceM, setDebounce]= useState(data);
+  useEffect(()=>{
+   let controller= setTimeout(()=>{
+    setDebounce(data)
+
+   },delay)
+
+    return ()=>{
+      clearTimeout(controller)
+    }
+
+  },[data,delay])
+
+
+  return debounceM
+
+
+}
+
+
+function App() {
+  const[data, setdata]=useState("")
+  const Debounced= useDebounce(data, 1000)
+
+  function DataInp(e){
+    setdata(e.target.value)
+
+  }
+
+  useEffect(()=>{
+    console.log("Expensive Operation")
+  },[Debounced])
+  return(
+    <div>
+
+      <input type='text' onChange={DataInp}></input>
+
+    </div>
+  )
+  
+}
+
+
+
+
+
+
+
+export default App
